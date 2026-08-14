22fe5f419f99e8485960497d307d28a5d269c46f
cuamll <callum.gray.10@ucl.ac.uk>
2026-08-14 17:25:47 +0100

    getting somewhere with generating protein params
    probably an incredibly ugly way to do this but it's working for now.
    need to tidy up the data member and also figure out a way to use the
    same main window for each step, like a wizard with multiple steps. also
    need to make a grid layout for annihilation remainders

--- gui.py
+++ gui.py
@@ -5,319 +5,82 @@
 
 import sys
 import random
+import numpy as np
 from PyQt6 import QtCore, QtWidgets, QtGui
 from PyQt6.QtCore import Qt
 import json
 from PyQt6.QtWidgets import *
 from typing import Any
 
-class TreeItem:
-    """A Json item corresponding to a line in QTreeView"""
-
-    def __init__(self, parent: "TreeItem" = None):
-        self._parent = parent
-        self._key = ""
-        self._value = ""
-        self._value_type = None
-        self._children = []
-
-    def appendChild(self, item: "TreeItem"):
-        """Add item as a child"""
-        self._children.append(item)
-
-    def child(self, row: int) -> "TreeItem":
-        """Return the child of the current item from the given row"""
-        return self._children[row]
-
-    def parent(self) -> "TreeItem":
-        """Return the parent of the current item"""
-        return self._parent
-
-    def childCount(self) -> int:
-        """Return the number of children of the current item"""
-        return len(self._children)
-
-    def row(self) -> int:
-        """Return the row where the current item occupies in the parent"""
-        return self._parent._children.index(self) if self._parent else 0
-
-    @property
-    def key(self) -> str:
-        """Return the key name"""
-        return self._key
-
-    @key.setter
-    def key(self, key: str):
-        """Set key name of the current item"""
-        self._key = key
-
-    @property
-    def value(self) -> str:
-        """Return the value name of the current item"""
-        return self._value
-
-    @value.setter
-    def value(self, value: str):
-        """Set value name of the current item"""
-        self._value = value
-
-    @property
-    def value_type(self):
-        """Return the python type of the item's value."""
-        return self._value_type
-
-    @value_type.setter
-    def value_type(self, value):
-        """Set the python type of the item's value."""
-        self._value_type = value
-
-    @classmethod
-    def load(
-        cls, value: list | dict, parent: "TreeItem" = None, sort=True
-    ) -> "TreeItem":
-        """Create a 'root' TreeItem from a nested list or a nested dictonary
-
-        Examples:
-            with open("file.json") as file:
-                data = json.dump(file)
-                root = TreeItem.load(data)
-
-        This method is a recursive function that calls itself.
-
-        Returns:
-            TreeItem: TreeItem
-        """
-        rootItem = TreeItem(parent)
-        rootItem.key = "root"
-
-        if isinstance(value, dict):
-            items = sorted(value.items()) if sort else value.items()
-
-            for key, value in items:
-                child = cls.load(value, rootItem)
-                child.key = key
-                child.value_type = type(value)
-                rootItem.appendChild(child)
-
-        elif isinstance(value, list):
-            for index, value in enumerate(value):
-                child = cls.load(value, rootItem)
-                child.key = index
-                child.value_type = type(value)
-                rootItem.appendChild(child)
-
-        else:
-            rootItem.value = value
-            rootItem.value_type = type(value)
-
-        return rootItem
-
-class JsonModel(QAbstractItemModel):
-    """ An editable model of Json data """
-
-    def __init__(self, parent: QObject = None):
-        super().__init__(parent)
-
-        self._rootItem = TreeItem()
-        self._headers = ("key", "value")
-
-    def clear(self):
-        """ Clear data from the model """
-        self.load({})
-
-    def load(self, document: dict):
-        """Load model from a nested dictionary returned by json.loads()
-
-        Arguments:
-            document (dict): JSON-compatible dictionary
-        """
-
-        assert isinstance(
-            document, (dict, list, tuple)
-        ), "`document` must be of dict, list or tuple, " f"not {type(document)}"
-
-        self.beginResetModel()
-
-        self._rootItem = TreeItem.load(document)
-        self._rootItem.value_type = type(document)
-
-        self.endResetModel()
-
-        return True
-
-    def data(self, index: QModelIndex, role: Qt.ItemDataRole) -> Any:
-        """Override from QAbstractItemModel
-
-        Return data from a json item according index and role
-
-        """
-        ret = None
-        if index.isValid():
-            item = index.internalPointer()
-            match role:
-                case Qt.ItemDataRole.DisplayRole:
-                    match index.column():
-                        case 0:
-                            ret = item.key
-                        case 1:
-                            ret = item.value
-                case Qt.ItemDataRole.EditRole:
-                    if index.column() == 1:
-                        ret = item.value
-        return ret
-
-    def setData(self, index: QModelIndex, value: Any, role: Qt.ItemDataRole):
-        """Override from QAbstractItemModel
-
-        Set json item according index and role
-
-        Args:
-            index (QModelIndex)
-            value (Any)
-            role (Qt.ItemDataRole)
-
-        """
-        if role == Qt.ItemDataRole.EditRole:
-            if index.column() == 1:
-                item = index.internalPointer()
-                item.value = str(value)
-
-                self.dataChanged.emit(index, index, [Qt.ItemDataRole.EditRole])
-
-                return True
-
-        return False
-
-    def headerData(
-        self, section: int, orientation: Qt.Orientation, role: Qt.ItemDataRole
-    ):
-        """Override from QAbstractItemModel
-
-        For the JsonModel, it returns only data for columns (orientation = Horizontal)
-
-        """
-        if role != Qt.ItemDataRole.DisplayRole:
-            return None
-
-        if orientation == Qt.Orientation.Horizontal:
-            return self._headers[section]
-
-    def index(self, row: int, column: int, parent=QModelIndex()) -> QModelIndex:
-        """Override from QAbstractItemModel
-
-        Return index according row, column and parent
-
-        """
-        if not self.hasIndex(row, column, parent):
-            return QModelIndex()
-
-        if not parent.isValid():
-            parentItem = self._rootItem
-        else:
-            parentItem = parent.internalPointer()
-
-        childItem = parentItem.child(row)
-        if childItem:
-            return self.createIndex(row, column, childItem)
-        else:
-            return QModelIndex()
-
-    def parent(self, index: QModelIndex) -> QModelIndex:
-        """Override from QAbstractItemModel
-
-        Return parent index of index
-
-        """
-
-        if not index.isValid():
-            return QModelIndex()
-
-        childItem = index.internalPointer()
-        parentItem = childItem.parent()
-
-        if parentItem == self._rootItem:
-            return QModelIndex()
-
-        return self.createIndex(parentItem.row(), 0, parentItem)
-
-    def rowCount(self, parent=QModelIndex()):
-        """Override from QAbstractItemModel
-
-        Return row count from parent index
-        """
-        if parent.column() > 0:
-            return 0
-
-        if not parent.isValid():
-            parentItem = self._rootItem
-        else:
-            parentItem = parent.internalPointer()
-
-        return parentItem.childCount()
-
-    def columnCount(self, parent=QModelIndex()):
-        """Override from QAbstractItemModel
-
-        Return column number. For the model, it always return 2 columns
-        """
-        return 2
-
-    def flags(self, index: QModelIndex) -> Qt.ItemFlags:
-        """Override from QAbstractItemModel
-
-        Return flags of index
-        """
-        flags = super(JsonModel, self).flags(index)
-
-        if index.column() == 1:
-            return Qt.ItemFlag.ItemIsEditable | flags
-        else:
-            return flags
-
-    def to_json(self, item=None):
-
-        if item is None:
-            item = self._rootItem
-
-        nchild = item.childCount()
-
-        if item.value_type is dict:
-            document = {}
-            for i in range(nchild):
-                ch = item.child(i)
-                document[ch.key] = self.to_json(ch)
-            return document
-
-        elif item.value_type == list:
-            document = []
-            for i in range(nchild):
-                ch = item.child(i)
-                document.append(self.to_json(ch))
-            return document
-
-        else:
-            return item.value
-
 class MatrixTable(QTableWidget):
-    def __init__(self, table_name, state_names, block_diagonal=False):
+    def __init__(self, data, block_diagonal=False):
         QTableWidget.__init__(self)
-        if type(table_name) is not str:
-            raise TypeError("Table name passed to MatrixTable must be str.")
-        if type(state_names) is not list:
-            raise TypeError("State names passed to MatrixTable must be list.")
-        self.table_name = table_name
-        self.state_names = state_names
-        self.n_states = len(self.state_names)
+        self.data = data
+        self.n_states = self.data["n_states"]
         self.setRowCount(self.n_states)
         self.setColumnCount(self.n_states)
-        self.setHorizontalHeaderLabels(self.state_names)
-        self.setVerticalHeaderLabels(self.state_names)
+        self.setHorizontalHeaderLabels(self.data["state_names"])
+        self.setVerticalHeaderLabels(self.data["state_names"])
         if block_diagonal:
             for i in range(self.n_states):
                 item = QtWidgets.QTableWidgetItem()
                 item.setBackground(QtGui.QColor("darkGray"))
                 item.setFlags(Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsEditable)
                 self.setItem(i, i, item)
+
+class RemainderTable(QTableWidget):
+    def __init__(self, data):
+        QTableWidget.__init__(self)
+        self.data = data
+        self.n_states = self.data["n_states"]
+        self.setRowCount(self.n_states)
+        self.setColumnCount(self.n_states)
+        self.setHorizontalHeaderLabels(self.data["state_names"])
+        self.setVerticalHeaderLabels(self.data["state_names"])
+        for i in range(self.n_states):
+            for j in range(self.n_states):
+                item = QtWidgets.QComboBox()
+                for k in range(self.n_states):
+                    item.addItem(self.data["state_names"][i])
+                self.setItem(i, j, item)
+
+class MatrixTables(QtWidgets.QWidget):
+    def __init__(self, data, parent=None):
+        QtWidgets.QWidget.__init__(self, parent=parent)
+        layout = QVBoxLayout(self)
+        self.data = data
+        print(f"MT init: self.data = {self.data}")
+        self.intra = MatrixTable(self.data, block_diagonal=True)
+        self.ann   = MatrixTable(self.data)
+        # self.ann_rem = RemainderTable(self.data)
+        layout.addWidget(self.intra)
+        layout.addWidget(self.ann)
+        # layout.addWidget(self.ann_rem)
+
+        btnNext = QtWidgets.QPushButton("Next")
+        btnNext.clicked.connect(self.showNext)
+        btnLayout = QHBoxLayout()
+        btnLayout.addWidget(btnNext)
+
+        layout.addLayout(btnLayout)
+        self.setLayout(layout)
+
+    def updateData(self):
+        intra = np.zeros((self.data["n_states"], self.data["n_states"]), dtype=float)
+        ann = np.zeros_like(intra)
+        ann_rem = np.zeros((self.data["n_states"], self.data["n_states"]), dtype=int)
+        for i in range(self.data["n_states"]):
+            for j in range(self.data["n_states"]):
+                intra[i, j] = float(self.intra.item(i, j).text())
+                ann[i, j] = float(self.ann.item(i, j).text())
+                # ann_rem[i, j] = self.ann_rem.item(i, j).currentIndex()
+        self.data["intra"] = intra
+        self.data["ann"] = ann
+        self.data["ann_remainder"] = ann_rem
+        print(f"MT exit: self.data = {self.data}")
+
+    def showNext(self):
+        self.updateData()
 
 class NameNumber(QtWidgets.QWidget):
     def __init__(self, data, parent=None):
@@ -342,13 +105,13 @@ class NameNumber(QtWidgets.QWidget):
         data["n_pigments"] = self.n_p.value()
         data["n_states"] = self.n_s.value()
 
-
 class NamePigmentsStates(QtWidgets.QWidget):
     def __init__(self, data, parent=None):
         QtWidgets.QWidget.__init__(self, parent=parent)
-        layout = QVBoxLayout(self)
-        pl = QGridLayout(self)
-        sl = QGridLayout(self)
+        layout = QVBoxLayout()
+        pl = QGridLayout()
+        sl = QGridLayout()
+        self.pigmentProperties = None
         self.pigment_names = []
         self.state_names = []
         if "n_pigments" in data:
@@ -359,24 +122,104 @@ class NamePigmentsStates(QtWidgets.QWidget):
             self.n_s = data["n_states"]
         else:
             self.n_s = 0
-        print(f"data = {data}")
+        print(f"NPS init: data = {data}")
+        self.data = data
+        print(f"NPS init: self.data = {self.data}")
 
         for i in range(self.n_p):
-            pl.addWidget(QLabel(f"Name of pigment {i:d}:"), i, 0)
+            pl.addWidget(QLabel(f"Name of pigment {i + 1:d}:"), i, 0)
             self.pigment_names.append(QLineEdit())
             pl.addWidget(self.pigment_names[i], i, 1)
         for i in range(self.n_s):
-            sl.addWidget(QLabel(f"Name of state {i:d}:"), i, 0)
+            sl.addWidget(QLabel(f"Name of state {i + 1:d}:"), i, 0)
             self.state_names.append(QLineEdit())
             sl.addWidget(self.state_names[i], i, 1)
-            sl.addWidget(QComboBox(), i, 2)
         layout.addLayout(pl)
         layout.addLayout(sl)
 
-    def updateData(self, data):
-        data["pigment_names"] = [p.text() for p in self.pigment_names]
-        data["state_names"] = [s.text() for s in self.state_names]
+        btnNext = QtWidgets.QPushButton("Next")
+        btnNext.clicked.connect(self.showPigmentProperties)
+        btnLayout = QHBoxLayout()
+        btnLayout.addWidget(btnNext)
 
+        layout.addLayout(btnLayout)
+        self.setLayout(layout)
+
+    def updateData(self):
+        self.data["pigment_names"] = [p.text() for p in self.pigment_names]
+        self.data["state_names"]   = [s.text() for s in self.state_names]
+
+    def showPigmentProperties(self):
+        self.updateData()
+        if self.pigmentProperties is None:
+            self.pigmentProperties = PigmentProperties(self.data)
+        self.pigmentProperties.show()
+        print(f"NPS exit: self.data = {self.data}")
+
+class PigmentProperties(QtWidgets.QWidget):
+    def __init__(self, data, parent=None):
+        QtWidgets.QWidget.__init__(self, parent=parent)
+        layout = QVBoxLayout()
+        pl = QGridLayout()
+        self.hop = []
+        self.decay = []
+        self.xsec = []
+        self.emissive = []
+        self.which_p = []
+        print(f"PP init: data = {data}")
+        self.data = data
+        print(f"PP init: self.data = {self.data}")
+        self.matrixTables = None
+
+        # column headers
+        pl.addWidget(QLabel("Hopping time (s)"), 0, 1)
+        pl.addWidget(QLabel("Decay time (s)"), 0, 2)
+        pl.addWidget(QLabel("Cross-section (cm^{-1})"), 0, 3)
+        pl.addWidget(QLabel("Emissive decay?"), 0, 4)
+        pl.addWidget(QLabel("Pigment"), 0, 5)
+        for i in range(self.data["n_states"]):
+            row = i + 1
+            pl.addWidget(QLabel(f"{self.data['state_names'][i]}:"), row, 0)
+            self.hop.append(QLineEdit("0.0"))
+            pl.addWidget(self.hop[i], row, 1)
+            self.decay.append(QLineEdit("0.0"))
+            pl.addWidget(self.decay[i], row, 2)
+            self.xsec.append(QLineEdit("0.0"))
+            pl.addWidget(self.xsec[i], row, 3)
+            self.emissive.append(QCheckBox())
+            pl.addWidget(self.emissive[i], row, 4)
+            self.which_p.append(QComboBox())
+            for name in self.data['pigment_names']:
+                self.which_p[i].addItem(name)
+            pl.addWidget(self.which_p[i], row, 5)
+
+        layout.addLayout(pl)
+
+        btnNext = QtWidgets.QPushButton("Next")
+        btnNext.clicked.connect(self.showMatrixTables)
+        btnLayout = QHBoxLayout()
+        btnLayout.addWidget(btnNext)
+
+        layout.addLayout(btnLayout)
+        self.setLayout(layout)
+
+    def updateData(self):
+        self.data["hop"] = [float(p.text())
+                            if p.text() != '' else 0.0 for p in self.hop]
+        self.data["decay"] = [float(p.text())
+                            if p.text() != '' else 0.0 for p in self.decay]
+        self.data["xsec"]  = [float(p.text())
+                            if p.text() != '' else 0.0 for p in self.xsec]
+        self.data["emissive"] = [p.isChecked() for p in self.emissive]
+        self.data["which_pigment"] = [p.currentIndex() + 1
+                                      for p in self.which_p]
+
+    def showMatrixTables(self):
+        self.updateData()
+        if self.matrixTables is None:
+            self.matrixTables = MatrixTables(self.data)
+        self.matrixTables.show()
+        print(f"PP exit: self.data = {self.data}")
 
 class ProteinBuilder(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -384,37 +227,24 @@ class ProteinBuilder(QtWidgets.QWidget):
         self.data = {}
         layout = QVBoxLayout(self)
 
-        self.Stack = QtWidgets.QStackedWidget()
-        self.Stack.addWidget(NameNumber(self.data))
-        self.Stack.addWidget(NamePigmentsStates(self.data))
-        self.Stack.addWidget(MatrixTable("LHCII",
-                            ["Chl_S", "Chl_T", "Car_S", "Car_T"], True))
+        self.nameNumber = NameNumber(self.data)
+        self.namePigmentStates = None
+        self.pigmentProperties = None
+        layout.addWidget(self.nameNumber)
 
         btnNext = QtWidgets.QPushButton("Next")
-        btnNext.clicked.connect(self.onNext)
-        btnPrevious = QtWidgets.QPushButton("Previous")
-        btnPrevious.clicked.connect(self.onPrevious)
+        btnNext.clicked.connect(self.showNamePigmentStates)
         btnLayout = QHBoxLayout()
-        btnLayout.addWidget(btnPrevious)
         btnLayout.addWidget(btnNext)
 
-        layout.addWidget(self.Stack)
         layout.addLayout(btnLayout)
         self.setLayout(layout)
 
-    def onNext(self):
-        self.Stack.currentWidget().updateData(self.data)
-        nxi = self.Stack.currentIndex() + 1
-        self.Stack.widget(nxi).update()
-        self.Stack.setCurrentIndex(nxi)
-        print(f"data = {self.data}")
-
-    def onPrevious(self):
-        self.Stack.currentWidget().updateData(self.data)
-        pxi = self.Stack.currentIndex() - 1
-        self.Stack.widget(pxi).update()
-        self.Stack.setCurrentIndex(pxi)
-        print(f"data = {self.data}")
+    def showNamePigmentStates(self):
+        self.nameNumber.updateData(self.data)
+        if self.namePigmentStates is None:
+            self.namePigmentStates = NamePigmentsStates(self.data)
+        self.namePigmentStates.show()
 
 if __name__ == "__main__":
     app =QtWidgets.QApplication([])
