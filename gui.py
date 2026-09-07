3057de71f5a4f2f217c820a84182c161344f7ebe
cuamll <callum.gray.10@ucl.ac.uk>
2026-09-07 15:23:35 +0100

    protein builder mostly works. a few bits left to sort out
    it's now possible to go through the whole protein builder, load in
    existing JSON data, edit it and save it. I need to change the
    getOpenFilename bit on the final page to allow for a new filename to be
    entered, and also sort out the UI elements on the final page. also the
    cleanupPage() implementation for the matrix tables needs fixing, and
    titles/subtitles and tooltips and so on. but it's mostly done!

--- gui.py
+++ gui.py
@@ -32,7 +32,7 @@ class loadExisting(QWizardPage):
     def __init__(self, parent):
         QWizardPage.__init__(self, parent)
         self.parent = parent
-        self.setTitle("Protein Builder")
+        self.setTitle("Load existing protein data.")
         self.setSubTitle('''
 If you'd like to import existing protein data, you can do that here.
 Otherwise, click Next to start specifying the parameters of your protein.
@@ -41,20 +41,26 @@ Otherwise, click Next to start specifying the parameters of your protein.
     def initializePage(self):
         layout = QVBoxLayout()
         gl = QGridLayout()
-        self.filename = QLineEdit()
+        self.filename = QLineEdit(os.getcwd())
         self.load_success = False
         self.browseButton = QPushButton("Browse") 
+        self.browseButton.setToolTip("Search for a JSON file")
         self.browseButton.clicked.connect(self.onBrowseButton)
         self.loadButton = QPushButton("Load") 
+        self.loadButton.setToolTip("Click to import JSON data from this file")
         self.loadButton.clicked.connect(self.onLoadButton)
         self.proteinChooser = QComboBox()
         self.resetButton = QPushButton("Reset")
+        self.resetButton.setToolTip("Delete loaded JSON data and start again")
         self.resetButton.clicked.connect(self.onResetButton)
         gl.addWidget(QLabel("Filename:"), 0, 0)
         gl.addWidget(self.filename, 0, 1)
         gl.addWidget(self.browseButton, 0, 2)
         gl.addWidget(self.loadButton, 0, 3)
-        gl.addWidget(QLabel("Protein name:"), 1, 0)
+        self.plabel = QLabel("Protein name:")
+        self.plabel.setToolTip('''Give your protein a short, descriptive name.
+Will be used to generate output directory structure.''')
+        gl.addWidget(self.plabel, 1, 0)
         gl.addWidget(self.proteinChooser, 1, 1)
         gl.addWidget(self.resetButton, 1, 2)
         layout.addLayout(gl)
@@ -100,6 +106,7 @@ Otherwise, click Next to start specifying the parameters of your protein.
             name = self.proteinChooser.currentText()
             self.parent.data = self.data[name]
             self.parent.data['name'] = name
+            self.parent.data['filename'] = self.filename.text()
         else:
             self.parent.data = {}
 
@@ -143,6 +150,14 @@ class nameNumber(QWizardPage):
         else:
             self.n_s.setValue(0)
         self.n_s.setRange(self.n_p.value(), 20)
+
+    def cleanupPage(self):
+        self.protein_name.setText("")
+        self.n_p.setValue(0)
+        self.n_s.setValue(0)
+        if getattr(self, "fields", False):
+            for field in self.fields:
+                del self.parent.data[field]
 
     def updateData(self):
         '''
@@ -245,6 +260,9 @@ class namePigmentsStates(QWizardPage):
                 child = layout.takeAt(0)
                 if child.widget:
                     child.widget().deleteLater()
+        if getattr(self, "fields", False):
+            for field in self.fields:
+                del self.parent.data[field]
 
 
     def updateData(self):
@@ -252,6 +270,7 @@ class namePigmentsStates(QWizardPage):
                                         for p in self.pigment_names]
         self.parent.data["state_names"]   = [s.text()
                                         for s in self.state_names]
+        self.fields = ["pigment_names", "state_names"]
 
     def checkData(self):
         '''
@@ -325,11 +344,41 @@ class pigmentProperties(QWizardPage):
             self.pl.addWidget(self.xsec[i], row, 3)
             self.pl.addWidget(self.emissive[i], row, 4)
             # need to find out how to centre the checkboxes. it's annoying
-            # self.pl.setAlignment(self.emissive[i], Qt.AlignHCenter)
+            self.pl.setAlignment(self.emissive[i],
+                                 Qt.AlignmentFlag.AlignHCenter)
             for j in range(self.field("n_p")):
                 name = self.parent.data["pigment_names"][j]
                 self.which_p[i].addItem(name)
             self.pl.addWidget(self.which_p[i], row, 5)
+        '''
+        check if there's data loaded and fill values if so
+        '''
+        keys = ["hop", "xsec"]
+        boxlists = [self.hop, self.xsec]
+        for k, b in zip(keys, boxlists):
+            if k in self.parent.data:
+                names = self.parent.data[k]
+            else:
+                names = [0.0 for _ in range(self.n_s)]
+            for i, n in enumerate(names):
+                b[i].setText(str(n))
+        if "intra" in self.parent.data:
+            for i in range(self.n_s):
+                self.decay[i].setText(str(self.parent.data["intra"][i][i]))
+        if "emissive" in self.parent.data:
+            ea = self.parent.data["emissive"]
+        else:
+            ea = [False for _ in range(self.n_s)]
+        if "which_pigment" in self.parent.data:
+            which = self.parent.data["which_pigment"]
+        else:
+            # python's 0-based and fortran is 1-based
+            # so the conversion has to be done somewhere; i do it
+            # on the python side and put the 1-based arrays in JSON
+            which = [1 for _ in range(self.n_s)]
+        for i in range(self.n_s):
+            self.emissive[i].setChecked(ea[i])
+            self.which_p[i].setCurrentIndex(which[i] - 1)
 
     def cleanupPage(self):
         while self.pl.count():
@@ -342,6 +391,10 @@ class pigmentProperties(QWizardPage):
         self.emissive = []
         self.which_p  = []
         self.n_s = 0
+        self.n_p = 0
+        if getattr(self, "fields", False):
+            for field in self.fields:
+                del self.parent.data[field]
 
     def updateData(self):
         self.parent.data["hop"] = [float(p.text())
@@ -362,7 +415,13 @@ class pigmentProperties(QWizardPage):
         validated = True
         msgs = []
         data = self.parent.data
-
+        if not(any(data["emissive"])):
+            validated = False
+            msgs.append("At least one decay should be emissive!")
+        print(f"xsec = {data['xsec']}, comp = {data['xsec'] == 0.0}")
+        if all([d == 0.0 for d in data['xsec']]):
+            validated = False
+            msgs.append("At least one state should have a non-zero cross section.")
         return validated, msgs
         
     def validatePage(self):
@@ -377,24 +436,30 @@ class pigmentProperties(QWizardPage):
         print(f"PP exit: data = {self.parent.data}")
         return validated
 
-    def cleanupPage(self):
-        '''
-        if the user goes back, delete the fields we've
-        added here so that there's no problem with duplicates
-        '''
-        if getattr(self, "fields", False):
-            for field in self.fields:
-                del self.parent.data[field]
-
 class MatrixTable(QTableWidget):
-    def __init__(self, state_names, block_diagonal=False):
+    def __init__(self, data, key, symmetric=False, block_diagonal=False):
         QTableWidget.__init__(self)
-        self.state_names = state_names
-        self.n_states = len(state_names)
+        self.state_names = data["state_names"]
+        self.n_states = len(self.state_names)
+        self.data = data
         self.setRowCount(self.n_states)
         self.setColumnCount(self.n_states)
         self.setHorizontalHeaderLabels(self.state_names)
         self.setVerticalHeaderLabels(self.state_names)
+        for i in range(self.n_states):
+            for j in range(self.n_states):
+                item = QTableWidgetItem()
+                if key in self.data:
+                    item.setText(str(self.data[key][i][j]))
+                self.setItem(i, j, item)
+        if symmetric:
+            for i in range(self.n_states):
+                for j in range(self.n_states):
+                    if i > j:
+                        item = QtWidgets.QTableWidgetItem()
+                        item.setBackground(QtGui.QColor("darkGray"))
+                        item.setFlags(Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsEditable)
+                        self.setItem(i, j, item)
         if block_diagonal:
             for i in range(self.n_states):
                 item = QtWidgets.QTableWidgetItem()
@@ -402,51 +467,223 @@ class MatrixTable(QTableWidget):
                 item.setFlags(Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsEditable)
                 self.setItem(i, i, item)
 
-class RemainderTable(QTableWidget):
-    def __init__(self, state_names):
-        QTableWidget.__init__(self)
-        self.state_names = state_names
-        self.n_states = len(state_names)
-        self.setRowCount(self.n_states)
-        self.setColumnCount(self.n_states)
-        self.setHorizontalHeaderLabels(self.state_names)
-        self.setVerticalHeaderLabels(self.state_names)
+class RemainderTable(QGridLayout):
+    def __init__(self, data):
+        QGridLayout.__init__(self)
+        self.state_names = data["state_names"]
+        self.n_states = len(self.state_names)
+        self.data = data
         for i in range(self.n_states):
+            self.addWidget(QLabel(self.state_names[i]), 0, i + 1)
+            self.addWidget(QLabel(self.state_names[i]), i + 1, 0)
             for j in range(self.n_states):
-                item = QtWidgets.QComboBox()
+                current = QtWidgets.QComboBox()
+                current.addItem("None")
                 for k in range(self.n_states):
-                    item.addItem(self.state_names[i])
-                self.setItem(i, j, item)
+                    current.addItem(self.state_names[k])
+                self.addWidget(current, i + 1, j + 1)
+                if "ann_remainder" in data:
+                    # 0 is the None index is the item list
+                    # so we don't need to mess about here
+                    ci = self.data["ann_remainder"][i][j]
+                    current.setCurrentIndex(ci)
+
+    def cleanup(self):
+        while self.count():
+            child = self.takeAt(0)
+            if child.widget:
+                child.widget().deleteLater()
 
 class matrixTables(QWizardPage):
     def __init__(self, parent):
         QWizardPage.__init__(self, parent)
+        self.parent = parent
+
+    def initializePage(self):
+        self.layout = QVBoxLayout()
+        self.intra = MatrixTable(self.parent.data, "intra", block_diagonal=True)
+        self.ann   = MatrixTable(self.parent.data, "ann", symmetric=True)
+        self.ann_rem_layout = RemainderTable(self.parent.data)
+        self.intra_label = QLabel("Transfer times between states (s)")
+        self.layout.addWidget(self.intra_label)
+        self.layout.addWidget(self.intra)
+        self.ann_label = QLabel("Annihilation times between states (s)")
+        self.layout.addWidget(self.ann_label)
+        self.layout.addWidget(self.ann)
+        self.ann_rem_label = QLabel("Remaining state after annihilation event")
+        self.layout.addWidget(self.ann_rem_label)
+        self.layout.addLayout(self.ann_rem_layout)
+        self.setLayout(self.layout)
+
+    def cleanupPage(self):
+        self.ann_rem_layout.cleanup()
+        while self.layout.count():
+            child = self.layout.takeAt(0)
+            print(child)
+            if child.widget:
+                child.widget().deleteLater()
+        self.intra  = []
+        self.ann    = []
+        self.intra_label = None
+        self.ann_label = None
+        if getattr(self, "fields", False):
+            for field in self.fields:
+                del self.parent.data[field]
+
+    def updateData(self):
+        n_s = self.parent.data["n_s"]
+        intra = np.zeros((n_s, n_s), dtype=float)
+        ann = np.zeros_like(intra)
+        ann_rem = np.zeros((n_s, n_s), dtype=int)
+        for i in range(n_s):
+            for j in range(n_s):
+                if i == j:
+                    intra[i, i] = self.parent.data['decay'][i]
+                else:
+                    intra[i, j] = float(self.intra.item(i, j).text())
+                if j >= i:
+                    # annihilation matrix must be symmetric
+                    ann[i, j] = float(self.ann.item(i, j).text())
+                    ann[j, i] = float(self.ann.item(i, j).text())
+                current = self.ann_rem_layout.itemAtPosition(i + 1, j + 1)
+                # None is always added as the first (0) index
+                ann_rem[i, j] = current.widget().currentIndex()
+        self.parent.data["intra"] = intra.tolist()
+        self.parent.data["ann"] = ann.tolist()
+        self.parent.data["ann_remainder"] = ann_rem.tolist()
+        self.fields = ["intra", "ann", "ann_remainder"]
+        print(f"MT exit: data = {self.parent.data}")
+
+    def checkData(self):
+        '''
+        check the entered data to make sure it's all tickety boo
+        '''
+        validated = True
+        msgs = []
+        data = self.parent.data
+        intra = data["intra"]
+        ann = data["ann"]
+        ann_rem = data["ann_remainder"]
+        for i in range(data["n_s"]):
+            for j in range(data["n_s"]):
+                s1 = data["state_names"][i]
+                s2 = data["state_names"][j]
+                if intra[i][j] < 0.0:
+                    msgs.append(f"Transfer times from {s1} to {s2} cannot be < 0.")
+                    validated = False
+                if ann[i][j] < 0.0:
+                    msgs.append(f"Annihilation time for {s1} and {s2} cannot be < 0.")
+                    validated = False
+                if ann[i][j] != ann[j][i]:
+                    msgs.append("Annihilation matrix is not symmetric.")
+                    validated = False
+                if ann_rem[i][j] < 0:
+                    msgs.append(f"Annihilation remainder for {s1} and {s2} invalid.")
+                    validated = False
+                if ann[i][j] == 0.0 and ann_rem[i][j] > 0:
+                    msgs.append(f"States {s1} and {s2} have an annihilation remainder set but a zero annihilation rate.")
+                    validated = False
+        return validated, msgs
+
+    def validatePage(self):
+        '''
+        update the parent's data, check it, print the current
+        dict for my benefit, then carry on if all is well
+        '''
+        self.updateData()
+        validated, msgs = self.checkData()
+        if not validated:
+            print(msgs)
+        print(f"PP exit: data = {self.parent.data}")
+        return validated
+
+class savePage(QWizardPage):
+    def __init__(self, parent):
+        QWizardPage.__init__(self, parent)
+        self.parent = parent
+        self.setTitle("Save")
+        self.setSubTitle("Save protein data to file.")
 
     def initializePage(self):
         layout = QVBoxLayout()
-        state_names = [self.field(f"state_name{i + 1:d}")
-                       for i in range(self.field("n_s"))]
-        intra = MatrixTable(state_names, block_diagonal=True)
-        ann   = MatrixTable(state_names)
-        ann_rem = RemainderTable(state_names)
-        layout.addWidget(intra)
-        layout.addWidget(ann)
-        layout.addWidget(ann_rem)
+        gl = QGridLayout()
+        if 'filename' in self.parent.data:
+            self.filename = QLineEdit(self.parent.data['filename'])
+        else:
+            self.filename = QLineEdit(os.getcwd())
+        self.save_success = False
+        self.load_success = False
+        self.existing_data = {}
+        self.browseButton = QPushButton("Browse") 
+        self.browseButton.setToolTip("Search for a JSON file")
+        self.browseButton.clicked.connect(self.onBrowseButton)
+        self.oaLabel = QLabel("Overwrite existing data or append to it?")
+        self.overwriteButton = QRadioButton()
+        self.appendButton = QRadioButton()
+        self.saveButton = QPushButton("Save") 
+        self.saveButton.setToolTip("Click to save JSON data to this file")
+        self.saveButton.clicked.connect(self.onSaveButton)
+        self.proteinChooser = QComboBox()
+        gl.addWidget(QLabel("Filename:"), 0, 0)
+        gl.addWidget(self.filename, 0, 1)
+        gl.addWidget(self.browseButton, 0, 2)
+        gl.addWidget(self.oaLabel, 1, 0)
+        gl.addWidget(self.overwriteButton, 2, 0)
+        gl.addWidget(self.appendButton, 2, 1)
+        gl.addWidget(self.saveButton, 2, 2)
+        layout.addLayout(gl)
         self.setLayout(layout)
 
+    def save_to_file(self):
+        dd = self.parent.data
+        del dd['filename']
+        del dd['decay']
+        name = dd.pop('name')
+        final_data = {name: dd}
+        success = True
+
+        if self.overwriteButton.isChecked():
+            with open(self.filename.text(), "w") as f:
+                try:
+                    json.dump(final_data, f)
+                except:
+                    print("Failed to save data to JSON.")
+                    success = False
+        elif self.appendButton.isChecked():
+            with open(self.filename.text(), "r") as f:
+                try:
+                    self.existing_data = json.load(f)
+                    print(self.existing_data)
+                    self.load_success = True
+                except:
+                    self.existing_data = {}
+                    print("Failed to load existing protein data from JSON.")
+                    self.load_success = False
+            if self.parent.data["name"] in self.existing_data.keys():
+                # need a qmessagebox for this
+                print("Protein name already exists in data file. Overwrite?")
+                overwrite = True
+                if overwrite:
+                    self.total_data = self.existing_data | final_data
+                else:
+                    success = False
+            else:
+                final_data = self.existing_data | final_data
+                with open(self.filename.text(), "w") as f:
+                    json.dump(final_data, f)
+        return success
+
+    def onBrowseButton(self):
+        self.fn, _ = QFileDialog.getOpenFileName(self, "Select JSON file",
+                                              os.getcwd(),
+                                              "JSON file (*.json)")
+        self.filename.setText(self.fn)
+
+    def onSaveButton(self):
+        self.save_success = self.save_to_file()
+
     def validatePage(self):
-        intra = np.zeros((self.data["n_states"], self.data["n_states"]), dtype=float)
-        ann = np.zeros_like(intra)
-        ann_rem = np.zeros((self.data["n_states"], self.data["n_states"]), dtype=int)
-        for i in range(self.data["n_states"]):
-            for j in range(self.data["n_states"]):
-                intra[i, j] = float(self.intra.item(i, j).text())
-                ann[i, j] = float(self.ann.item(i, j).text())
-                # ann_rem[i, j] = self.ann_rem.item(i, j).currentIndex()
-        self.parent.data["intra"] = intra
-        self.parent.data["ann"] = ann
-        self.parent.data["ann_remainder"] = ann_rem
-        print(f"MT exit: data = {self.parent.data}")
+        return self.save_success
 
 class ProteinBuilder(QWizard):
     def __init__(self):
@@ -457,7 +694,8 @@ class ProteinBuilder(QWizard):
         self.addPage(namePigmentsStates(self))
         self.addPage(pigmentProperties(self))
         self.addPage(matrixTables(self))
-
+        self.addPage(savePage(self))
+        self.setWindowTitle("Protein builder for STOP")
 
 if __name__ == "__main__":
     app =QtWidgets.QApplication([])
